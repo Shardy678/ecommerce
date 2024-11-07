e57e1bfbe8110e7a361a70da428e14a73ede3af1
Eugene <tambov.j678@gmail.com>
2024-11-07 16:40:06 +0300

    setup mongodb with mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,11 @@
 const express = require('express')
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+const Cat = mongoose.model('Cat', { name: String });
+
+const kitty = new Cat({ name: 'Zildjian' });
+kitty.save().then(() => console.log('meow'));
 const app = express()
 const port = 3000
 
