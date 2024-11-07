3d072a386692332f3eccd0185ba8a7d7573b6fdf
Eugene <tambov.j678@gmail.com>
2024-11-07 17:08:51 +0300

    setup auth with jwt

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,43 @@
 const express = require('express')
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken')
 mongoose.connect('mongodb://127.0.0.1:27017/test');
 
+const User = mongoose.model('User', { username: String, password: String });
 const Cat = mongoose.model('Cat', { name: String });
 
 const kitty = new Cat({ name: 'Zildjian' });
 kitty.save().then(() => console.log('meow'));
 const app = express()
 const port = 3000
+const JWT_SECRET = 'mysecret'
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+        return res.status(400).json({ error: 'User already exists' });
+    }
+    const newUser = new User({ username, password });
+    await newUser.save();
+    const token = jwt.sign({ username }, JWT_SECRET);
+    res.json({ token });
+})
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user || user.password !== password) {
+        return res.status(401).json({ error: 'Invalid credentials' });
+    }
+    const token = jwt.sign({ username }, JWT_SECRET);
+    res.json({ token });
 })
 
 app.listen(port, () => {
